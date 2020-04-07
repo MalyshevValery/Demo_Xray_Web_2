8a0bb95640521b133d76472307122dce11650971
Malyshev Valery <malyshevalery@gmail.com>
2020-04-07 15:27:18 +0300

    Initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
+import 'zone.js';
+import 'zone.js/dist/long-stack-trace-zone.js';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+
+import { ImageBrowserComponent } from './image-browser/image-browser.component';
+import { ImageAnalyseComponent } from './image-analyse/image-analyse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageBrowserComponent,
+    ImageAnalyseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
