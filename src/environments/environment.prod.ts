7766359ed2f9297f46a2b31a793ed0294b0a0560
Malyshev Valery <malyshevalery@gmail.com>
2020-11-13 15:46:56 +0300

    Prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://grid.hopto.org/xray_api',
+  kratosUrl: 'https://grid.hopto.org/kratos',
+  selfUrl: 'https://grid.hopto.org/xray_lung'
 };
